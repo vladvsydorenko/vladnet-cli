088a528cb45bcbd12c44b6de6184b6073946d951
Vlad Sydorenko <vladv.sydorenko@gmail.com>
2018-09-14 10:44:11 +0300

    Add errors

--- src/bin/helpers.ts
+++ src/bin/helpers.ts
@@ -14,13 +14,23 @@ export const normalizeActionPaths = (paths: string[]) => {
 export const loadActions = (config: ICLIConfig) => {
     const absoluteActionPaths = normalizeActionPaths(config.actions);
 
-    const actions = absoluteActionPaths.reduce((acc, actionPath) => {
-        const loadedActions = nativeRequire(actionPath);
-        Object
-            .keys(loadedActions.actions)
-            .forEach(key => {
-                acc[key] = loadedActions.actions[key];
-            });
+    const actions = absoluteActionPaths.reduce((acc, actionPath, i) => {
+        let loadedActions: any;
+
+        try {
+            loadedActions = nativeRequire(actionPath);
+        }
+        catch(err) {
+            console.error(`[vladnet] Error while reading ${config.actions[i]}`, err.stack);
+        }
+
+        if (loadActions) {
+            Object
+                .keys(loadedActions.actions)
+                .forEach(key => {
+                    acc[key] = loadedActions.actions[key];
+                });
+        }
 
         return acc;
     }, {} as { [key: string]: ICliAction });
