e5656beb7a94b6b125d4ded0b5ff536ad2881d5c
Vlad Sydorenko <vladv.sydorenko@gmail.com>
2018-09-14 11:19:58 +0300

    add errors

--- src/bin/bin_vladnet.ts
+++ src/bin/bin_vladnet.ts
@@ -6,15 +6,36 @@ import { loadActions } from "./loadActions";
 
 const configPath = path.join(process.cwd(), "vladnet.json");
 
-try {
-    const config = <ICLIConfig>JSON.parse(fs.readFileSync(configPath).toString());
-    const actions = loadActions(config);
-    const command = process.argv[2];
-    const action = actions[command];
-    argvParser.option(action.options);
-    const result = argvParser.run(process.argv.slice(3));
-    action.run(result.options);
+const read = () => {
+    try {
+        const config = <ICLIConfig>JSON.parse(fs.readFileSync(configPath).toString());
+        const actions = loadActions(config);
+        const command = process.argv[2];
+
+        if (!command) {
+            console.error(`No action name passed`);
+            return;
+        }
+
+        const action = actions[command];
+
+        if (!action) {
+            console.error(`Action "${command}" not found`);
+            return;
+        }
+
+        if (!Array.isArray(action.options) || typeof action.run !== "function") {
+            console.error(`Loaded action "${command}" has wrong type, should have "options" and "run"`);
+            return;
+        }
+
+        argvParser.option(action.options);
+        const result = argvParser.run(process.argv.slice(3));
+        action.run(result.options);
+    }
+    catch(err) {
+        console.error(`Error reading config by path "${configPath}"`, err.stack);
+    }
 }
-catch(err) {
-    console.error(`Error reading config by path "${configPath}"`, err.stack);
-}
+
+read();
